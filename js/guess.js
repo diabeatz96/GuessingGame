9c030bade55920ddae0676d6eca732f2c11bdf14
Estemaux <esteban.cuellaraguilar.us@gmail.com>
2022-01-22 15:33:59 -0500

    Updated point score
    Line

--- js/guess.js
+++ js/guess.js
@@ -1,46 +1,40 @@
-// This is a game
-// alert ('Hello');
-
-
-// const some_array = [1, 'John', 2, 'Peter', true, false];
-
-// for (let i = 0; i < some_array[lenght]; i++){
-//     if (typeof some_array [i] =='string') continue;
-//         console.log(some_array [i], typeof some_array [i])
-// }
-
-// for(let i = some_array.length-1; i >= 0; i--){
-//     if (typeof some_array[i] == 'string') continue;
-//     console.log('Looping Backwards: ', some_array[i], typeof some_array[i])
-// }
-
-// Project 3
-
-
-//This is a new comment;
-
 alert ('This is a game website');
+
+// Number guessing
 
 var y = Math.floor(Math.random() * 100 + 1);
 
 var guess = 1;
 
-document.getElementById("submit_guess").onclick = function(){
+document.getElementById("submit_guess").onclick = function()
+{
 
-var x = document.getElementById("guess_field").value;
+   var x = document.getElementById("guess_field").value;
 
-if(x == y)
-{
-   alert("Great! You guessed the number right in "
-           + guess + " guess! ");
+   // Point system
+   // starting score 
+   var score = 10; 
+
+   if(x == y)
+      {
+         document.getElementById('score').innerHTML = score;
+         alert("Great! You guessed the number right in " + guess + " guess! Your score is " + score + "points" );
+      }
+   else if(x > y)
+      {
+         guess++;
+         alert("Not a winner! Choose a smaller number!");
+         document.getElementById("score").innerHTML = score - 1;
+      }
+   else if (x < y)
+      {
+         guess++;
+         alert("Not a winner! Choose a greater number!");
+         document.getElementById("score").innerHTML = score - 1;
+      }
 }
-else if(x > y)
+
+if (score == 0)
 {
-   guess++;
-   alert("Not a winner! Choose a smaller number!");
-}
-else
-{
-   guess++;
-   alert("Not a winner! Choose a greater number!")
+   alert("You lost! Try again")
 }
